67edd0993769193e6966c5c84fc2c5c248f0a708
rubiii <me@rubiii.com>
2013-05-12 18:54:56 +0200

    conventions

--- spec/geotrust_spec.rb
+++ spec/geotrust_spec.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 describe Nokogiri do
 
   it "doesn't forget element namespaces" do
-    xml = File.read('geotrust.wsdl')
+    xml = File.read File.expand_path('../geotrust.wsdl', __FILE__)
     document = Nokogiri.XML(xml)
 
     element = document.at_xpath('//xs:schema/xs:element[@name="GetQuickApproverList"]/xs:complexType/xs:sequence/xs:element[@name="Request"]', 'xs' => 'http://www.w3.org/2001/XMLSchema')
